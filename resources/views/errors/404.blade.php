ffad035b8c7be1fb6abbbb6e96151327ccbac884
Leo Luchini <leonel.luchini@ec.gba.gov.ar>
2016-11-11 10:38:17 -0300

    cambio el html del 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,32 @@
-@extends('layouts.app_back')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <title>labDATA</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
 	<div class="container">
-		<h2>La página que está buscando no existe.</h2>
+	    <section class="cuadro-superior">
+	        <div class="contenedor_logos">
+	            <div class="col-xs-6">
+	                <a href="http://www.econo.unlp.edu.ar/laboratorio" target="_blank">
+	                    {!! Html::image('images/logo_laboratorio.png', 'Laboratorio de Desarrollo Sectorial y Territorial', ['class'=>'pull-right'])!!}
+	                </a>
+	            </div>
+	            <div class="col-xs-6 border_left">
+	                <a href="http://www.econo.unlp.edu.ar" target="_blank">
+	                    {!! Html::image('images/logo_facultad.png', 'Facultad de Ciencias Econ&oacute;micas', ['class'=>'pull-left'])!!}
+	                </a>
+	            </div>
+	        </div>
+	    </section>
+    	<center>
+    		<h2>La página que está buscando no existe.</h2>
+    	</center>
 	</div>
-@endsection
+</body>
+</html>
