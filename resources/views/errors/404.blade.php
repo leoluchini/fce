83cfcfd5ff3bb162257825aec1746d27c2627f78
Jeronimo Vieyra <jerodcv@gmail.com>
2016-11-17 08:44:01 -0300

    cambio en error 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,26 +7,38 @@
 
     <title>labDATA</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {!!Html::style('css/app.css')!!}
+
+        <!-- Fuente CUPRUM -->
+    <link href="{{ asset('css/cuprum.css') }}" rel="stylesheet" type="text/css">
 </head>
+
 <body>
-	<div class="container">
-	    <section class="cuadro-superior">
-	        <div class="contenedor_logos">
-	            <div class="col-xs-6">
-	                <a href="http://www.econo.unlp.edu.ar/laboratorio" target="_blank">
-	                    {!! Html::image('images/logo_laboratorio.png', 'Laboratorio de Desarrollo Sectorial y Territorial', ['class'=>'pull-right'])!!}
-	                </a>
-	            </div>
-	            <div class="col-xs-6 border_left">
-	                <a href="http://www.econo.unlp.edu.ar" target="_blank">
-	                    {!! Html::image('images/logo_facultad.png', 'Facultad de Ciencias Econ&oacute;micas', ['class'=>'pull-left'])!!}
-	                </a>
-	            </div>
-	        </div>
-	    </section>
-    	<center>
-    		<h2>La página que está buscando no existe.</h2>
-    	</center>
-	</div>
+    <section class="cuadro-superior">
+        <div class="contenedor_logos">
+            <div class="col-xs-6">
+                <a href="http://www.econo.unlp.edu.ar/laboratorio" target="_blank">
+                    {!! Html::image('images/logo_laboratorio.png', 'Laboratorio de Desarrollo Sectorial y Territorial', ['class'=>'pull-right'])!!}
+                </a>
+            </div>
+            <div class="col-xs-6 border_left">
+                <a href="http://www.econo.unlp.edu.ar" target="_blank">
+                    {!! Html::image('images/logo_facultad.png', 'Facultad de Ciencias Econ&oacute;micas', ['class'=>'pull-left'])!!}
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="cuadro-inferior">
+        <div clas="col-xs-12">
+            <div class="marco_negro">
+                <p class="text-center texto_404">
+                    La página que está buscando no existe.
+                </p>
+            </div>
+        </div>
+    </section>
+
 </body>
 </html>
